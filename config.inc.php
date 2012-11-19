2dfae17649bfda2b32a7e8aaaf23b7074e5fbf83
RexDude <rexdude@arcor.de>
2012-11-19 17:34:27 +0100

    Removed functions.js and prepared for BE settings page

--- config.inc.php
+++ config.inc.php
@@ -11,6 +11,12 @@ $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
 
 $REX['PERM'][] = 'slice_status[]';
 
+// --- DYN
+$REX['ADDON']['slice_status']['slice_titlebar_background'] = '#dddddd';
+$REX['ADDON']['slice_status']['slice_content_background'] = '#e7e5e5';
+$REX['ADDON']['slice_status']['slice_content_opacity'] = '0.6';
+// --- /DYN
+
 require_once($REX['INCLUDE_PATH'] . '/addons/slice_status/functions/functions_slice_status.inc.php');
 
 if ($REX['REDAXO']) {
@@ -30,9 +36,37 @@ if ($REX['REDAXO']) {
 		//rex_generateAll(); 
 	}
 
-	// insert js file for coloring offline slices
-	$insert = '<script src="../files/addons/slice_status/functions.js" type="text/javascript"></script>' . "\r\n";
-	rex_register_extension('PAGE_HEADER', create_function('$params', 'return $params[\'subject\'] . \''. $insert . '\';'));
+	// insert js code for coloring offline slices
+	rex_register_extension('OUTPUT_FILTER', 'addJSCode');
+
+	function addJSCode($params) {
+		global $REX;
+		
+		$script = "
+		<!-- BEGIN slice_status -->
+		<script type=\"text/javascript\">
+			var sliceTitleBarBackground = '" . $REX['ADDON']['slice_status']['slice_titlebar_background'] . "';
+			var sliceContentBackground = '" . $REX['ADDON']['slice_status']['slice_content_background'] . "';
+			var sliceContentOpacity = " . $REX['ADDON']['slice_status']['slice_content_opacity'] . ";
+
+			jQuery(document).ready(function($) {
+				var jSliceTitleBar = $('.slice-status-offline').parents('.rex-content-editmode-module-name');
+				var jSliceContent = jSliceTitleBar.next('.rex-content-editmode-slice-output');
+
+				// titlebar
+				jSliceTitleBar.css('background', sliceTitleBarBackground);
+
+				// slice content
+				jSliceContent.wrap('<div style=\"background: ' + sliceContentBackground  + ';\" />');
+				jSliceContent.css('background', sliceContentBackground);
+				jSliceContent.css('opacity', sliceContentOpacity);
+			});
+		</script>
+		<!-- END slice_status -->
+		";
+
+		return str_replace('</body>', $script . "\r\n" . '</body>', $params['subject']);
+	}
 }
 
 rex_register_extension('SLICE_SHOW', 'sliceShow');
