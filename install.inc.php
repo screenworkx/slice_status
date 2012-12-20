fabf32d849b141b6ce4de0ff78a50a0eff2d07d2
RexDude <rexdude@arcor.de>
2012-12-20 12:27:08 +0100

    Issue #5 fixed

--- install.inc.php
+++ install.inc.php
@@ -9,10 +9,19 @@ $sql->setQuery('ALTER TABLE `' . $REX['TABLE_PREFIX'] . 'article_slice` ADD `sta
 $sql->setQuery('SELECT a356_is_online FROM `' . $REX['TABLE_PREFIX'] . 'article_slice`');
 
 if ($sql->getRows() > 0) {
-	// import data from slice_onoff row to slice_status row
+	// import data from slice_onoff column to slice_status column
 	$sql->setQuery('UPDATE `' . $REX['TABLE_PREFIX'] . 'article_slice` SET status = a356_is_online');
 
+	// import message
+	if (!$I18N->hasMsg('import_msg')) {
+		// this is for redaxo < 4.4
+		$I18N->appendFile($REX['INCLUDE_PATH'] . '/addons/slice_status/lang/');
+	}
+	
 	echo rex_info($I18N->msg('import_msg'));
+
+	// remove old slice_onoff querys from cached articles
+	rex_generateAll();
 }
 
 $REX['ADDON']['install']['slice_status'] = 1;
--- uninstall.inc.php
+++ uninstall.inc.php
@@ -3,5 +3,8 @@
 $sql = new rex_sql();
 $sql->setQuery('ALTER TABLE `' . $REX['TABLE_PREFIX'] . 'article_slice` DROP `status`');
 
+// delete cache
+rex_generateAll();
+
 $REX['ADDON']['install']['slice_status'] = 0;
 ?>
